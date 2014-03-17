7f7f77fe2c626fd132161aea107d03fecdde97b0
Marc Farra <marcfarra@gmail.com>
2014-03-17 05:10:56 -0400

    Deploy translations

--- dist/scripts/text_content.js
+++ dist/scripts/text_content.js
@@ -34,24 +34,24 @@ whiteBallotText.textEn = {
 
 /* The text content of the page in Arabic */
 whiteBallotText.textAr = {
-	title : 'قوة <fh>الاقتراع بيضاء</fh>',
-	desc : 'الاقتراع الأبيض يرمز إلى رفض قوي من النظام السياسي ونظام الحكم والقانون الانتخابي. أنه ليس لديه اعتراف الدينية وصوت الثابت الوحيد في لبنان.',
-	select: 'Select a map:',
-	attribution: 'Data by Lebanese Association for Democratic Elections',
+	title : 'قوة <fh>الاقتراع صوت أبيض</fh>',
+	desc : ' تظهر هذه الخريطة عدد الأوراق البيضاء وعدد الأوراق غير المحتسبة (الملغاة) في كل دائرة انتخابيّة بالنسبة لجميع الانتخابات النيابية. والمتفق عليه أن الورقة البيضاء ترمز إلى رفض القانون الانتخابي أو المرشحين. ',
+	select: ":اختر خريطة",
+	attribution: 'الجمعية اللبنانية من أجل ديمقراطية الإنتخابات',
 	content: [
 		{
 			ttime : '2005',
-			time_desc : 'Invalid votes of 2005 13994 votes'
+			time_desc : ' الأصوات الباطلة والبيضاء عام 2005 '
 			// need to add map layer
 		},
 		{
 			ttime : '2009',
-			time_desc : 'Invalid votes of 2009 11390 votes'
+			time_desc : 'الأصوات الباطلة عام 2009'
 			// need to add map layer
 		},
 		{
 			ttime : '2009',
-			time_desc : 'White votes of 2009 12522 votes'
+			time_desc : 'الأصوات البيضاء عام 2009'
 		}
 
 	]
@@ -64,13 +64,39 @@ confessions.textEn = {
 	// title: '<span style="font-family: serif !important;font-weight:700;font-size:0.8em"> The power of the </span><br> <span style="font-family:Helvetica Neue;font-weight: 100">W H I T E  .  B A L L O T</span>',
 	title: 'Registered voters by confession',
 	desc : 'The following map shows the variance (between two consecutive parliamentary elections) in the total number of the registered voters of each sect per electoral district and their percentages (in respect to the total number of voters in that district). Green indicates an increase in influence, while red indicates a decrease in influence.',
-	attribution: 'Data by Lebanese Association for Democratic Elections'
+	attribution: 'Data by Lebanese Association for Democratic Elections',
+	confession: "Select a confession",
+	year: "Select a year",
+    alawite: "Alawite",
+    armorth: "Armenian Orthodox",
+    armcth: "Armenian Catholic",
+    evang: "Evangelical Protestant",
+    minorities: "Minorities",
+    sunni: "Sunni",
+    shia: "Shia",
+    maronite: "Maronite",
+    druze: "Druze",
+    grkorth: "Greek Orthodox",
+    grkcth: "Greek Catholic",
 };
 
 confessions.textAr = {
-	title : 'قوة <fh>الاقتراع بيضاء</fh>',
-	desc : 'الاقتراع الأبيض يرمز إلى رفض قوي من النظام السياسي ونظام الحكم والقانون الانتخابي. أنه ليس لديه اعتراف الدينية وصوت الثابت الوحيد في لبنان.',
-	attribution: 'Data by Lebanese Association for Democratic Elections'
+	title : 'الناخبين المسجلين حسب الطائفة',
+	desc : 'تظهر هذه الخريطة الفارق (في دورتين انتخابيتين نيابيتين متتاليتين) بين العدد الجملي  للناخبين عن كل طائفة في كل دائرة انتخابية، كما تظهر النسب (بالنسبة لمجمل  عدد الناخبين عن هذه الدائرة). اللون الأخضر يشير إلى تزايد تأثير ناخبي هذه الطائفة، في حين أنّ اللون الأحمر يشير إلى انخفاض هذا التأثير.',
+	attribution: 'الجمعية اللبنانية من أجل ديمقراطية الإنتخابات',
+	confession: ":اختر طائفة",
+	year: ":اختر السنة",
+	alawite: "علوية",
+    armorth: "ارمن الارثوذكس",
+    armcth: "ارمن الكاثوليك ",
+    evang: "بروتستانتية ",
+    minorities: "الأقليات",
+    sunni: "سني",
+    shia: "شيعة",
+    maronite: "موارنة",
+    druze: "دروز",
+    grkorth: "روم الارثوذكس",
+    grkcth: "روم الكاثوليك",
 };
 
 
@@ -90,16 +116,16 @@ genderText.textEn = {
 };
 
 genderText.textAr = {
-	title : 'قوة <fh>الاقتراع بيضاء</fh>',
-	desc : 'الاقتراع الأبيض يرمز إلى رفض قوي من النظام السياسي ونظام الحكم والقانون الانتخابي. أنه ليس لديه اعتراف الدينية وصوت الثابت الوحيد في لبنان.',
-	attribution: 'Data by Lebanese Association for Democratic Elections',
-	select: "Select a map:",
-	voter_turnout: "Voter Turnout",
-	voter_registration: "Voter Registration",
-	f_maj1: "Female majority (> 1%)",
-    f_maj2: "Female majority (0% - 1%)",
-    m_maj1: "Male majority (0% - 1%)",
-    m_maj2: "Male majority (> 1%)"
+	title : 'الناخبين المسجلين حسب الجنس',
+	desc :'تظهر هذه الخريطة الفارق (في دورتين انتخابيتين نيابيتين متتاليتين) بين العدد الجملي  للناخبين الذكور والإناث (الناخبين المسجلين والمقترعين) في كل دائرة انتخابية. اللون البرتقالي يشير إلى أنّ عدد الذكور أكبر من عدد الإناث (سواء كانوا أعداد الناخبين المسجلين أم المقترعين). اللون البنفسجي يشير إلى أنّ عدد الإناث أكبر من عدد الذكور.',
+	attribution: 'الجمعية اللبنانية من أجل ديمقراطية الإنتخابات',
+	select: ":اختر خريطة",
+	voter_turnout: " الناخبين حسب المشاركة ",
+	voter_registration: "الناخبين حسب التسجيل",
+	f_maj1: "(> 1%)   الاغلبية إناث",
+    f_maj2: "(0% - 1%)   الاغلبية إناث",
+    m_maj1: "(0% - 1%)   الاغلبية ذكور",
+    m_maj2: " (> 1%)   الاغلبية ذكور"
 };
 
 // *************** Vote Power
@@ -110,24 +136,12 @@ powerText.textEn = {
 	'<br><br>The numbers shown are percentage of registered voters in the district out of total voters compared to percent of seats allocated for that district.' + 
 	'Ideally, 25,000 voters should be represented by one parliamentary member.',
 	attribution: 'Data by Lebanese Association for Democratic Elections',
-	select: "Select a map:",
-	voter_turnout: "Voter Turnout",
-	voter_registration: "Voter Registration",
-	f_maj1: "Female majority (> 1%)",
-    f_maj2: "Female majority (0% - 1%)",
-    m_maj1: "Male majority (0% - 1%)",
-    m_maj2: "Male majority (> 1%)"
+
 };
 
 powerText.textAr = {
-	title : 'قوة <fh>الاقتراع بيضاء</fh>',
-	desc : 'الاقتراع الأبيض يرمز إلى رفض قوي من النظام السياسي ونظام الحكم والقانون الانتخابي. أنه ليس لديه اعتراف الدينية وصوت الثابت الوحيد في لبنان.',
-	attribution: 'Data by Lebanese Association for Democratic Elections',
-	select: "Select a map:",
-	voter_turnout: "Voter Turnout",
-	voter_registration: "Voter Registration",
-	f_maj1: "Female majority (> 1%)",
-    f_maj2: "Female majority (0% - 1%)",
-    m_maj1: "Male majority (0% - 1%)",
-    m_maj2: "Male majority (> 1%)"
+	title : 'مساواة التمثيل بين الناخبين',
+	desc : ' تظهر هذه الخريطة تمثيل كل دائرة انتخابيّة في البرلمان.يتم الحصول على النتيجة من خلال قسمة عدد الناخبين المحتملين في كل دائرة انتخابيّة على كامل عدد الناخبين في كل لبنان وقسمة عدد المقاعد النيابيّة المرصودة لهذه الدائرة الانتخابية على كامل عدد المقاعد النيابيّة في لبنان  <br><br>. يشير اللون الأزرق إلى الدوائر التي يعتبر فيها التمثيل النيابي لهذه الدائرة ضئيلاً، في حين يشير اللون الأحمر إلى الدوائر التي يعتبر فيها التمثيل النيابي لهذه الدائرة مبالغا فيه.',
+	attribution: 'الجمعية اللبنانية من أجل ديمقراطية الإنتخابات',
+
 };

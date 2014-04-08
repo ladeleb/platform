68e0625b65184cf003d2f017965998cfab9d8b92
Marc Farra <marcfarra@gmail.com>
2014-04-08 17:54:29 +0300

    Add CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,7 +219,8 @@ module.exports = function (grunt) {
                         'scripts/*',
                         'styles/{,*/}*.css',
                         'bower_components/**',
-                        'data/**'
+                        'data/**',
+                        'CNAME'
                     ]
                 }]
             }
